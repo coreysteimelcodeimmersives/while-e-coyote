0f956e91d7d5b8e4886a03886fde60587d0e2226
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-02-09 14:01:02 -0700

    number summer program complete

---
+++ number-summer.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+console.log("Enter some numbers (type 'done' when complete): ")
+
+let num = prompt("> ");
+
+let total = 0;
+
+//condition 
+while (num !== 'done'){
+    let num2 = Number(num)
+    total = total + num2;
+    num = prompt("> ");
+}
+
+console.log("Your total is: " + total);
